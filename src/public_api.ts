f9d8c45549bfb6edc551f9565c0f5de81c8e89d3
iwe7 <admin@meepo.com.cn>
2018-06-06 15:31:50 +0800

    update

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,1 +1,6 @@
 export * from './lib/iwe7-jd.module';
+
+export { JdBoxModule } from './lib/jd-box/jd-box.module';
+export { JdSwiperModule } from './lib/jd-swiper/jd-swiper.module';
+export { JdSearchModule } from './lib/jd-search/jd-search.module';
+export { JdTipModule } from './lib/jd-tip/jd-tip.module';
