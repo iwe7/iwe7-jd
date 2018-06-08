3ab642a9d4052d9b94b87dff9c5c0e7f8aacb35f
iwe7 <admin@meepo.com.cn>
2018-06-08 16:48:49 +0800

    update

--- src/lib/jd-swiper/jd-swiper.ts
+++ src/lib/jd-swiper/jd-swiper.ts
@@ -12,7 +12,10 @@ import { Iwe7IcssService } from 'iwe7-icss';
 export class JdSwiperComponent extends Iwe7BaseComponent {
   @Input() list: any[] = [{
     image: 'https://m.360buyimg.com/mobilecms/s800x390_jfs/t20671/8/411797135/142904/501ef443/5b0d24ceN270c03f0.jpg!cr_1125x549_0_72!q70.jpg.dpg.webp'
+  }, {
+    image: 'https://m.360buyimg.com/mobilecms/s800x390_jfs/t20671/8/411797135/142904/501ef443/5b0d24ceN270c03f0.jpg!cr_1125x549_0_72!q70.jpg.dpg.webp'
   }];
+  
   height: any;
   constructor(injector: Injector) {
     super(injector, 'jd-swiper');
