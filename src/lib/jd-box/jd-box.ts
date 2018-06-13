89875efcb0f1e03dcc015edbe50e3fb5978f379f
iwe7 <admin@meepo.com.cn>
2018-06-13 12:53:36 +0800

    update

--- src/lib/jd-box/jd-box.ts
+++ src/lib/jd-box/jd-box.ts
@@ -1,18 +1,22 @@
+import { TemplateRef, ViewEncapsulation, ContentChild } from '@angular/core';
 import { Iwe7IcssService } from 'iwe7-icss';
 import { Iwe7BaseComponent } from 'iwe7-base';
-import { Component, OnInit, AfterViewInit, ElementRef, Input, Injector, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ElementRef, Input, Injector, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'jd-box',
   templateUrl: 'jd-box.html',
   styleUrls: ['./jd-box.scss'],
   providers: [Iwe7IcssService],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class JdBoxComponent extends Iwe7BaseComponent {
   width: string;
   @Input() icon: string;
   @Input() title: string;
+
+  @ContentChild(TemplateRef) content: TemplateRef<any>;
   constructor(public ele: ElementRef, injector: Injector) {
     super(injector, 'jd-box');
     this.runOutsideAngular(() => {
